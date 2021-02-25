bbf9681b16dce69a64965e7d44b38dd2a133cdce
Jan B <keksgesicht23@gmail.com>
2021-02-26 00:39:12 +0100

    Debug Nachrichten zu Algos

--- algo-computerräume.sh
+++ algo-computerräume.sh
@@ -3,6 +3,7 @@
 . ./settings.sh
 
 for rpc in $room_pc; do
+	echo "Raum $rpc wird nun vorbereitet."
 	awk -F ';' '$1 ~ /r'$rpc'/ {print}' $work_input > $work_output
 	roll_out
 done
--- algo-stockwerke.sh
+++ algo-stockwerke.sh
@@ -2,10 +2,12 @@
 
 . ./settings.sh
 
+echo "00er Räume"
 awk -F ';' '$2 ~ /^[0-9][0-9]pc/ {print}' $work_input > $work_output
 roll_out
 
 for i in $(seq 1 7); do
+	echo "$i00er Räume"
 	awk -F ';' '$2 ~ /^'$i'[0-9][0-9]pc/ {print}' $work_input > $work_output
 	roll_out
 done
